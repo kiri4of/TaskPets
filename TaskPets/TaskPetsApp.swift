8c376e2b3da0f44fd399917f6d9e5123ced853e8
Kiri4of <kopak7890@gmail.com>
2025-01-28 15:07:55 +0000

    init

---
+++ TaskPets/TaskPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPetsApp.swift
+//  TaskPets
+//
+//  Created by Kiri4of on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
